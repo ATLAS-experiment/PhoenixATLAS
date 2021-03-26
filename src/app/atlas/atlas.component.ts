7f88e22dbe5bb35f5fa613a878ffc63771b581f7
Fawad Ali <m.fawaadali98@gmail.com>
2021-03-26 23:08:14 +0500

    Update ATLAS geometries and config

--- src/app/atlas/atlas.component.ts
+++ src/app/atlas/atlas.component.ts
@@ -53,65 +53,210 @@ export class ATLASComponent implements OnInit {
     this.eventDisplay.init(configuration);
 
     // Load detector geometries
-    this.eventDisplay.loadOBJGeometry(
-      'assets/geometry/toroids.obj',
-      'Toroids',
-      0x8c8c8c,
-      undefined,
-      false,
+
+    // Magnets + Support
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Barrel-Toroid.gltf',
+      'Barrel Toroid',
+      'Magnets',
+      1000,
       false
     );
-    this.eventDisplay.loadOBJGeometry(
-      'assets/geometry/TRT.obj',
-      'TRT',
-      0x356aa5,
-      undefined,
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/End-Cap-Toroid.gltf',
+      'Endcap',
+      'Magnets',
+      1000,
       false
     );
-    this.eventDisplay.loadOBJGeometry(
-      'assets/geometry/SCT.obj',
-      'SCT',
-      0xfff400,
-      undefined,
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Feet.gltf',
+      'Feet',
+      'Magnets',
+      1000,
       false
     );
-    this.eventDisplay.loadOBJGeometry(
-      'assets/geometry/pixel.obj',
-      'Pixel',
-      0x356aa5,
-      undefined,
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Warm-Structure.gltf',
+      'Warm structure',
+      'Magnets',
+      1000,
       false
     );
-    this.eventDisplay.loadOBJGeometry(
-      'assets/geometry/LAR_Bar.obj',
+
+    // LAr
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Lar-Barrel.gltf',
       'LAr Barrel',
-      0x19ccd2,
-      undefined,
-      true,
+      'Calorimeters',
+      1000,
       false
     );
-    this.eventDisplay.loadOBJGeometry(
-      'assets/geometry/LAR_EC1.obj',
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Lar-EMEC.gltf',
       'LAr EC1',
-      0x19ccd2,
-      undefined,
-      true,
+      'Calorimeters',
+      1000,
       false
     );
-    this.eventDisplay.loadOBJGeometry(
-      'assets/geometry/LAR_EC2.obj',
-      'LAr EC2',
-      0x19ccd2,
-      undefined,
-      true,
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Lar-FCAL.gltf',
+      'LAr FCAL',
+      'Calorimeters',
+      1000,
       false
     );
-    this.eventDisplay.loadOBJGeometry(
-      'assets/geometry/TileCal.obj',
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Lar-HEC.gltf',
+      'LAr HEC',
+      'Calorimeters',
+      1000,
+      false
+    );
+
+    // Tile
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Tile-Barrel.gltf',
       'Tile Cal',
-      0xc14343,
-      undefined,
-      true,
+      'Calorimeters',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Tile-End-Cap.gltf',
+      'Tile Cal EC',
+      'Calorimeters',
+      1000,
+      false
+    );
+
+    // Inner Detector
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Beam.gltf',
+      'Beam',
+      'Inner Detector',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Pixel.gltf',
+      'Pixel',
+      'Inner Detector',
+      1000,
+      true
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/SCT-BAR.gltf',
+      'SCT',
+      'Inner Detector',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/SCT-EC.gltf',
+      'SCT Endcaps',
+      'Inner Detector',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/TRT-BAR.gltf',
+      'TRT',
+      'Inner Detector',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/TRT-EC.gltf',
+      'TRT Endcaps',
+      'Inner Detector',
+      1000,
+      false
+    );
+
+    // Muons
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Extra-Wheel.gltf',
+      'Extra wheel',
+      'Muon Spectrometer > Endcaps',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Big-Wheel.gltf',
+      'Big wheel',
+      'Muon Spectrometer > Endcaps',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Outer-Wheel.gltf',
+      'Outer Wheel',
+      'Muon Spectrometer > Endcaps',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Muon-Barrel-Inner.gltf',
+      'Muon Barrel Inner',
+      'Muon Spectrometer > Barrel',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Muon-Barrel-Middle.gltf',
+      'Muon Barrel Middle',
+      'Muon Spectrometer > Barrel',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Muon-Barrel-Outer.gltf',
+      'Muon Barrel Outer',
+      'Muon Spectrometer > Barrel',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Muon-Big-Wheel-MDT.gltf',
+      'Big Wheel MDT ',
+      'Muon Spectrometer > Endcaps',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Small-Wheel-Chambers.gltf',
+      'Small Wheel',
+      'Muon Spectrometer > Endcaps',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Small-Wheel-Hub.gltf',
+      'Small Wheel Hub',
+      'Muon Spectrometer > Endcaps',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/Small-Wheel-NJD.gltf',
+      'Small Wheel Feet',
+      'Muon Spectrometer > Endcaps',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/TGC2.gltf',
+      'TGC2',
+      'Muon Spectrometer > Endcaps',
+      1000,
+      false
+    );
+    this.eventDisplay.loadGLTFGeometry(
+      'assets/geometry/TGC3.gltf',
+      'TGC3',
+      'Muon Spectrometer > Endcaps',
+      1000,
       false
     );
 
