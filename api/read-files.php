867911a6841cab2f6ec241a2628e3d4a93265cda
Fawad Ali <m.fawaadali98@gmail.com>
2021-05-06 00:51:18 +0500

    Update PHP script to read config directory

--- api/read-files.php
+++ api/read-files.php
@@ -1,46 +1,43 @@
 <?php
+
+include_once('./helpers.php');
+
 // Do not report errors (comment for development).
 error_reporting(E_ERROR | E_PARSE);
 
 // Associative array / dictionary for configuration.
 $config = array(
-  'directory' => './data/'
+  'event_data' => '../data/event_data/',
+  'config' => '../data/config/'
 );
+
+// Set the configured directory to read for either event data or config.
+$directoryToRead = 'event_data';
+
+// Check if the type of data requested is config and set the the directory to read.
+if (isset($_GET['type']) && $_GET['type'] == 'config') {
+  $directoryToRead = 'config';
+}
 
 // If the `f` query param is set then read the file specified through the param.
 if (isset($_GET['f'])) {
-  echo file_get_contents($config['directory'] . $_GET['f']);
+  echo file_get_contents($config[$directoryToRead] . $_GET['f']);
   return;
 }
 
-// Read all files in the given directory. The directory is relative to this PHP script.
-function readAllFilesInDirectory($dir)
-{
-  $dirIterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
-  $filesArray = array();
-
-  foreach ($dirIterator as $path) {
-    if (!$path->isDir()) {
-      $filesArray[] = str_replace('\\', '/', $path->getPathname());
-    }
-  }
-
-  return $filesArray;
-}
-
 // Throw a 404 if the configured directory is not found.
-if (!is_dir($config['directory'])) {
+if (!is_dir($config[$directoryToRead])) {
   http_response_code(404);
   return;
 }
 
 // Read all the files in the configured directory.
-$allFiles = readAllFilesInDirectory($config['directory']);
+$allFiles = readAllFilesInDirectory($config[$directoryToRead]);
 
 // Prepare the response and remove `./data` at the start of files paths.
 $response = array_map(
-  function ($file) use ($config) {
-    return substr($file, strlen($config['directory']));
+  function ($file) use ($config, $directoryToRead) {
+    return substr($file, strlen($config[$directoryToRead]));
   },
   $allFiles
 );
